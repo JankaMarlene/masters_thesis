695534159d6b0af74c3dab6712219850bae5a1b1
Janka Hauffe <138319402+JankaMarlene@users.noreply.github.com>
2024-04-17 11:58:30 +0200

    Create K-means.R

---
+++ scripts/K-means.R
@@ -0,0 +1,94 @@
+# K-means
+library(tidyverse)
+library(dplyr)
+library(dendextend)
+load("clean_data.RData")
+
+# Extract relevant columns from clean_data
+cog_df <- clean_data[, c("group","pvt_reaction_time","nback_miss_1","nback_miss_2","tmt_a_time","tmt_b_time")]
+# Check structure and summarize contents of cog_df
+str(cog_df)
+summary(cog_df)
+
+# Store group labels in a separate variable and exclude label (group column) from the dataset to do clustering
+# Later true labels will be used to check how good clustering turned out
+cog_label <- cog_df$group
+cog_df$group <- NULL
+str(cog_df)
+
+## k-means
+set.seed(123)
+km.out <- kmeans(cog_df, centers = 4, nstart = 20)
+km.out
+
+# Decide how many clusters to look at
+n_clusters <- 10
+
+# Initialize total within sum of squares error: wss
+wss <- numeric(n_clusters)
+
+set.seed(123)
+
+# Look over 1 to n possible clusters
+for (i in 1:n_clusters) {
+  # Fit the model: km.out
+  km.out <- kmeans(cog_df, centers = i, nstart = 20)
+  # Save the within cluster sum of squares
+  wss[i] <- km.out$tot.withinss
+}
+
+# Produce a screen plot
+wss_df <- tibble(clusters = 1:n_clusters, wss = wss)
+
+scree_plot <- ggplot(wss_df, aes(x = clusters, y = wss, group = 1)) +
+  geom_point(size = 4) +
+  geom_line() +
+  scale_x_continuous(breaks = c(2, 4, 6, 8, 10)) +
+  xlab('Number of clusters')
+scree_plot
+
+scree_plot +
+  geom_hline(
+    yintercept = wss, 
+    linetype = 'dashed', 
+    col = c(rep('#000000',3),'#FF0000', rep('#000000', 6))
+  )
+
+# Select number of clusters
+k <- 4
+set.seed(123)
+
+# Build model with k clusters: km.out
+km.out <- kmeans(cog_df, centers = k, nstart = 20)
+
+# Append cluster results obtained back in the original dataframe
+cog_df_cl <- mutate(cog_df, cluster = km.out$cluster)
+
+# Count how many observations were assigned to each cluster with the count function
+count(cog_df_cl, cluster)
+
+# Cross-checking clustering results using table function
+table(cog_df_cl$cluster, cog_label)
+
+
+# Prepare data (count of observations in each cluster for each label category)
+cluster_label_counts <- table(cog_label, cog_df_cl$cluster)
+
+# Create bar plot with custom y-axis range and ticks
+barplot(cluster_label_counts, beside = TRUE, col = c("blue", "red"), 
+        main = "Cluster Assignments by Label Category", xlab = "Cluster", ylab = "Count",
+        ylim = c(0, 20),
+        axes = FALSE)
+
+# Add y-axis with custom ticks
+axis(side = 2, at = seq(0, 20, by = 1))  # Add ticks at every count from 0 to 20
+
+# Add legend
+legend("topright", legend = c("withoutPCS", "withPCS"), fill = c("blue", "red"))
+
+# Only possible with two variables, but I have more
+# cog_subset_clean_cog$cluster_id <- factor(km.out$cluster)
+# ggplot(cog_subset_clean_cog, aes(number_of_reviews, price, color = cluster_id)) +
+# geom_point(alpha = 0.25) +
+# xlab("Number of reviews") +
+# ylab("Price")

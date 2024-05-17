bb00d924f201e509e377c28b5628069b4358f5dc
Janka Hauffe <138319402+JankaMarlene@users.noreply.github.com>
2024-05-17 13:24:20 +0200

    Update Hierarchical_clustering.R

--- scripts/Hierarchical_clustering.R
+++ scripts/Hierarchical_clustering.R
@@ -290,3 +290,60 @@ t_test_cluster_2 <- t.test(facit_f_FS ~ group, data = cluster_2_data)
 # Display t-test results
 t_test_cluster_2
 
+#-------
+  
+# Build distance matrix
+# Since all values are continuous numerical values I use euclidean distance method
+dist_mat <- dist(cog_df, method = 'euclidean')
+
+# Now decide which linkage method to use
+# Try different kinds of linkage methods after decide which performed better
+# Build dendrogram by plotting hierarchical cluster object with hclust
+# Specify linkage method via 'method' argument
+hclust_median <- hclust(dist_mat, method = 'median')
+plot(hclust_median)
+
+# Create the desired number of clusters
+# Since I want two groups 'withPCS' and 'withoutPCS' number of clusters = 2
+cut_median <- cutree(hclust_median, k = 2)
+# To visualize clusters on dendrogram use abline function to draw the cut line
+plot(hclust_median)
+rect.hclust(hclust_median, k = 2, border = 2:40)
+abline(h = 28, col = 'red')
+# Visualize tree with different colored branches
+median_dend_obj <- as.dendrogram(hclust_median)
+median_col_dend <- color_branches(median_dend_obj, h = 28)
+plot(median_col_dend)
+
+# Visualize the clusters see YT Video Hierarchical Clustering in R Spencer Pao
+# install.packages("factoextra")
+# library(factoextra)
+# cluster_obj <- list(data = cog_df_sc, cluster = cut_avg)
+# fviz_cluster(cluster_obj)
+# rownames(cog_df_sc) <- paste(cog_label, 1:dim(cog_df) [1], sep = "_")
+# fviz_cluster(list(data=cog_df_sc))
+
+# Append cluster results obtained back in the original dataframe 
+# Use mutate
+# Count how many observations were assigned to each cluster with the count function
+cog_df_cl <- mutate(cog_df, cluster = cut_median)
+count(cog_df_cl,cluster)
+
+# Cross-checking clustering results using table funcion
+table(cog_df_cl$cluster,cog_label)
+
+# Add the cluster information from cog_df_cl to clean_data
+clean_data$cluster <- cog_df_cl$cluster
+
+# Check the updated structure of clean_data
+str(clean_data)
+
+#------------
+
+library(fossil)
+
+# Calculate the Adjusted Rand Index
+ari_median_ward <- adj.rand.index(cut_median, cut_ward)
+
+# Print the ARI
+print(ari_median_ward)

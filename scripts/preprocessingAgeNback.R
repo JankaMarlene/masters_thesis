52243461b625f8390a6bdf679be37e4fe0ff7fad
Janka Hauffe <138319402+JankaMarlene@users.noreply.github.com>
2024-06-24 09:54:08 +0200

    Update preprocessingAgeNback.R

--- scripts/preprocessingAgeNback.R
+++ scripts/preprocessingAgeNback.R
@@ -6,7 +6,10 @@ alldata <- read.delim("C:/Users/jankj/OneDrive/Desktop/masters_thesis/data/parti
 
 # Subset of alldata that contains only the important variables
 subset <- alldata %>%
-  select(participant_id,nr, age, sex, group, graduation, years_of_education, neurological_diseases_1, facit_f_FS, hads_a_total_score, hads_d_total_score, psqi_total_score, moca, pvt_reaction_time, nback_miss_1, nback_false_alarm_1 ,nback_miss_2 ,nback_false_alarm_2 ,tmt_a_time, tmt_b_time)
+  select(participant_id,nr, age, sex, group, graduation, years_of_education, neurological_diseases_1, facit_f_FS, hads_a_total_score, hads_d_total_score, psqi_total_score, moca, pvt_reaction_time, nback_miss_1, nback_false_alarm_1 ,nback_miss_2 ,nback_false_alarm_2 ,tmt_a_time, tmt_b_time) %>%
+  mutate(tmt_diff = tmt_b_time - tmt_a_time)
+  
+# Adding the TMT difference
 
 # Convert 'moca' variable to a binary variable based on a cutoff score of 25 
 # 1 represents scores less than 26 (may) indicating cognitive impairment 
@@ -16,7 +19,7 @@ subset <- subset %>%
 
 # Subset with all relevant cognitive data
 cog_subset <- subset %>%
-  select(participant_id,nr, age, group, pvt_reaction_time, nback_miss_1, nback_false_alarm_1, nback_miss_2, nback_false_alarm_2, tmt_a_time, tmt_b_time)
+  select(participant_id,nr, age, group, pvt_reaction_time, nback_miss_1, nback_false_alarm_1, nback_miss_2, nback_false_alarm_2, tmt_a_time, tmt_b_time, tmt_diff)
 
 # Summarize cog_subset
 cog_subset %>%
@@ -100,7 +103,7 @@ print(clean_data)
 
 # Test correlation
 clean_data |>
-  select(pvt_reaction_time, nback_miss_1, nback_false_alarm_1, nback_miss_2, nback_false_alarm_2, tmt_a_time, tmt_b_time) |>
+  select(pvt_reaction_time, nback_miss_1, nback_false_alarm_1, nback_miss_2, nback_false_alarm_2, tmt_a_time, tmt_b_time, tmt_diff) |>
   cor(use = "pairwise.complete.obs") |>
   round(2)
 # Scatter plot of tmt_a_time vs. tmt_b_time faceted by age
@@ -124,16 +127,35 @@ create_age_groups <- function(age) {
   }
 }
 
+# Function to create age groups for tmt_diff
+create_age_groups_tmt_diff <- function(age) {
+  if (age >= 18 & age <= 24) {
+    return("18-24 Years")
+  } else if (age >= 25 & age <= 54) {
+    return("25-54 Years")
+  } else if (age >= 55 & age <= 64) {
+    return("55-64 Years")
+  } else if (age >= 65 & age <= 80) {
+    return("65-80 Years")
+  }
+}
 
 # Create age groups
 clean_data <- clean_data %>%
   mutate(
-    age_group = sapply(age, create_age_groups))
+    age_group = sapply(age, create_age_groups),
+    age_group_tmt_diff = sapply(age, create_age_groups_tmt_diff)
+    )
 
 # Calculate mean and standard deviation for each age group and variable
 age_group_summary <- clean_data %>%
   group_by(age_group) %>%
   summarize(across(c("pvt_reaction_time", "tmt_a_time", "tmt_b_time"), 
+                   list(mean = mean, sd = sd)))
+
+age_group_summary_tmt_diff <- clean_data %>%
+  group_by(age_group_tmt_diff) %>%
+  summarize(across(c("tmt_diff"), 
                    list(mean = mean, sd = sd)))
 
 # Function to calculate z-scores
@@ -142,20 +164,23 @@ calculate_z_scores <- function(x, mean, sd) {
 }
 
 # Function to calculate z-scores for each individual based on age for all variables
-calculate_z_scores_individual <- function(x, age, age_group_summary) {
+calculate_z_scores_individual <- function(x, age, age_group_summary, age_group_summary_tmt_diff) {
   # Find the corresponding age group for each individual
   age_group <- sapply(age, create_age_groups)
+  age_group_tmt_diff <- sapply(age, create_age_groups_tmt_diff)
   
   # Join the age group summary data to the individual data based on age group
   individual_data <- data.frame(x, age, age_group) %>%
-    left_join(age_group_summary, by = "age_group")
+    left_join(age_group_summary, by = "age_group") %>%
+    left_join(age_group_summary_tmt_diff, by = "age_group_tmt_diff")
   
   # Calculate z-scores for all variables
   z_scores <- individual_data %>%
     mutate(
       z_pvt_reaction_time = calculate_z_scores(pvt_reaction_time, pvt_reaction_time_mean, pvt_reaction_time_sd),
       z_tmt_a_time = calculate_z_scores(tmt_a_time, tmt_a_time_mean, tmt_a_time_sd),
-      z_tmt_b_time = calculate_z_scores(tmt_b_time, tmt_b_time_mean, tmt_b_time_sd)
+      z_tmt_b_time = calculate_z_scores(tmt_b_time, tmt_b_time_mean, tmt_b_time_sd),
+      z_tmt_diff = calculate_z_scores(tmt_diff, tmt_diff_mean, tmt_diff_sd)
     ) %>%
     select(starts_with("z_"))
   
@@ -166,7 +191,7 @@ calculate_z_scores_individual <- function(x, age, age_group_summary) {
 }
 
 # Calculate z-scores for each individual based on age for all variables
-clean_data <- calculate_z_scores_individual(clean_data, clean_data$age, age_group_summary)
+clean_data <- calculate_z_scores_individual(clean_data, clean_data$age, age_group_summary, age_group_summary_tmt_diff)
 
 # Scale the variables nback_miss_1 and nback_miss_2 and add them as new columns
 clean_data <- clean_data %>%

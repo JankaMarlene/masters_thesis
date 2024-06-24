09ef1f20587f4f6b60eb914acf1bcf7742e10c43
Janka Hauffe <138319402+JankaMarlene@users.noreply.github.com>
2024-06-24 18:53:41 +0200

    Update Hierarchical_clustering.R

--- scripts/Hierarchical_clustering.R
+++ scripts/Hierarchical_clustering.R
@@ -7,7 +7,7 @@ library(gridExtra)
 load("clean_data.RData")
 
 # Extract relevant columns from clean_data
-cog_df <- clean_data[, c("group","z_pvt_reaction_time","z_tmt_a_time","z_tmt_b_time",)]
+cog_df <- clean_data[, c("group","z_pvt_reaction_time","z_tmt_a_time","z_tmt_b_time")]
 # Check structure and summarize contents of cog_df
 str(cog_df)
 summary(cog_df)
@@ -67,6 +67,38 @@ str(clean_data)
 # Checking sex in cluster
 table(cog_df_cl$cluster,clean_data$sex)
 
+# Checking education in cluster
+cluster_means <- tapply(clean_data$years_of_education, cog_df_cl$cluster, FUN = mean)
+cluster_means
+table(cog_df_cl$cluster,clean_data$years_of_education)
+
+# T-Test for years of education between clusters overall
+t_test_education <- t.test(years_of_education ~ as.factor(cog_df_cl$cluster), data = clean_data)
+
+# Display t-Test results
+t_test_education
+
+# T-Test for years of education within the "withPCS" group between clusters
+t_test_withPCS_education <- t.test(years_of_education ~ as.factor(cluster), data = subset(clean_data, group == "withPCS"))
+
+# Display t-Test results for the "withPCS" group
+t_test_withPCS_education
+
+# T-Test for years of education within the "withoutPCS" group between clusters
+t_test_withoutPCS_education <- t.test(years_of_education ~ as.factor(cluster), data = subset(clean_data, group == "withoutPCS"))
+
+# Display t-Test results for the "withoutPCS" group
+t_test_withoutPCS_education
+
+# Plotting years of education distribution within clusters based on withPCS and withoutPCS labels with mean as text
+ggplot(clean_data, aes(x = as.factor(cog_df_cl$cluster), y = years_of_education, fill = group)) +
+  geom_boxplot(position = position_dodge(width = 0.75)) + # Adjust position of boxplots
+  stat_summary(fun = mean, geom = "point", position = position_dodge(width = 0.75), # Adjust position of points
+               shape = 18, size = 4, color = "red") + # Add mean point
+  stat_summary(fun = mean, geom = "text", aes(label = round(after_stat(y), 1)), 
+               position = position_dodge(width = 0.75), vjust = -0.5) + # Add mean as text
+  labs(x = "Cluster", y = "years_of_education", title = "years_of_education Distribution within Clusters based on withPCS and withoutPCS Labels")
+
 # Plotting age distribution between clusters with mean as text
 ggplot(clean_data, aes(x = as.factor(cog_df_cl$cluster), y = age)) +
   geom_boxplot() +
@@ -103,19 +135,19 @@ t_test_withPCS
 t_test_withoutPCS
 
 # Filter the data for Cluster 2
-cluster_4_data <- subset(clean_data, cluster == 2)
+#cluster_2_data <- subset(clean_data, cluster == 2)
 
 # Perform t-test for facit_f_FS between "withPCS" and "withoutPCS" groups within Cluster 2
-t_test_cluster_4 <- t.test(age ~ group, data = cluster_4_data)
+#t_test_cluster_2 <- t.test(age ~ group, data = cluster_2_data)
 
 # Display t-test results
-t_test_cluster_4
+#t_test_cluster_2
 
 
 
 
 # Vector of variables for which to create boxplots
-variables <- c("z_pvt_reaction_time", "s_nback_miss_1", "s_nback_miss_2", "z_tmt_a_time", "z_tmt_b_time","z_tmt_diff")
+variables <- c("pvt_reaction_time", "nback_miss_1", "nback_miss_2", "tmt_a_time", "tmt_b_time","tmt_diff")
 
 # Initialize an empty list to store the plots
 plot_list <- list()
@@ -206,10 +238,12 @@ test_results_withoutPCS <- list()
 # Loop over each variable
 for (variable in variables) {
   # Perform t-test for the current variable within "withPCS" group
-  t_test_result_withPCS <- t.test(clean_data$age[clean_data$group == "withPCS"], clean_data[[variable]][clean_data$group == "withPCS"])
+  t_test_result_withPCS <- t.test(clean_data[[variable]][clean_data$group == "withPCS" & cog_df_cl$cluster == 1],
+                           clean_data[[variable]][clean_data$group == "withPCS" & cog_df_cl$cluster == 2])
   
   # Perform t-test for the current variable within "withoutPCS" group
-  t_test_result_withoutPCS <- t.test(clean_data$age[clean_data$group == "withoutPCS"], clean_data[[variable]][clean_data$group == "withoutPCS"])
+  t_test_result_withoutPCS <- t.test(clean_data[[variable]][clean_data$group == "withoutPCS" & cog_df_cl$cluster == 1],
+                              clean_data[[variable]][clean_data$group == "withoutPCS" & cog_df_cl$cluster == 2])
   
   # Store the test results for withPCS group in the list
   test_results_withPCS[[variable]] <- t_test_result_withPCS
@@ -317,39 +351,39 @@ t_test_cluster_2
 
 
 # Filter the data for Cluster 2
-cluster_4_data <- subset(clean_data, cluster == 4)
+#cluster_4_data <- subset(clean_data, cluster == 4)
 
 # Perform t-test for facit_f_FS between "withPCS" and "withoutPCS" groups within Cluster 2
-t_test_cluster_4 <- t.test(facit_f_FS ~ group, data = cluster_4_data)
+#t_test_cluster_4 <- t.test(facit_f_FS ~ group, data = cluster_4_data)
 
 # Display t-test results
-t_test_cluster_4
+#t_test_cluster_4
 
 
 #-------
   
 # Build distance matrix
 # Since all values are continuous numerical values I use euclidean distance method
-dist_mat <- dist(cog_df, method = 'euclidean')
+#dist_mat <- dist(cog_df, method = 'euclidean')
 
 # Now decide which linkage method to use
 # Try different kinds of linkage methods after decide which performed better
 # Build dendrogram by plotting hierarchical cluster object with hclust
 # Specify linkage method via 'method' argument
-hclust_median <- hclust(dist_mat, method = 'median')
-plot(hclust_median)
+#hclust_median <- hclust(dist_mat, method = 'median')
+#plot(hclust_median)
 
 # Create the desired number of clusters
 # Since I want two groups 'withPCS' and 'withoutPCS' number of clusters = 2
-cut_median <- cutree(hclust_median, k = 2)
+#cut_median <- cutree(hclust_median, k = 2)
 # To visualize clusters on dendrogram use abline function to draw the cut line
-plot(hclust_median)
-rect.hclust(hclust_median, k = 2, border = 2:40)
-abline(h = 28, col = 'red')
+#plot(hclust_median)
+#rect.hclust(hclust_median, k = 2, border = 2:40)
+#abline(h = 28, col = 'red')
 # Visualize tree with different colored branches
-median_dend_obj <- as.dendrogram(hclust_median)
-median_col_dend <- color_branches(median_dend_obj, h = 28)
-plot(median_col_dend)
+#median_dend_obj <- as.dendrogram(hclust_median)
+#median_col_dend <- color_branches(median_dend_obj, h = 28)
+#plot(median_col_dend)
 
 # Visualize the clusters see YT Video Hierarchical Clustering in R Spencer Pao
 # install.packages("factoextra")
@@ -362,27 +396,27 @@ plot(median_col_dend)
 # Append cluster results obtained back in the original dataframe 
 # Use mutate
 # Count how many observations were assigned to each cluster with the count function
-cog_df_cl <- mutate(cog_df, cluster = cut_median)
-count(cog_df_cl,cluster)
+#cog_df_cl <- mutate(cog_df, cluster = cut_median)
+#count(cog_df_cl,cluster)
 
 # Cross-checking clustering results using table funcion
-table(cog_df_cl$cluster,cog_label)
+#table(cog_df_cl$cluster,cog_label)
 
 # Add the cluster information from cog_df_cl to clean_data
-clean_data$cluster <- cog_df_cl$cluster
+#clean_data$cluster <- cog_df_cl$cluster
 
 # Check the updated structure of clean_data
-str(clean_data)
+#str(clean_data)
 
 #------------
 
-library(fossil)
+#library(fossil)
 
 # Calculate the Adjusted Rand Index
-ari_median_ward <- rand.index(cut_median, cut_ward)
+#ari_median_ward <- rand.index(cut_median, cut_ward)
 
 # Print the ARI
-print(ari_median_ward)
+#print(ari_median_ward)
 
 
 #-------

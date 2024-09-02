ce779c8b21f716ad35189f0e0d849117a5ccff66
Janka Hauffe <138319402+JankaMarlene@users.noreply.github.com>
2024-09-02 09:37:00 +0200

    tidy up scripts

--- scripts/Visualize_power.m
+++ scripts/Visualize_power.m
@@ -1,103 +1,141 @@
 %% Script to visualize the power data (topoplots and power spectrum)
+% overview
+% 0. Preliminaries
+% 1. topoplots
+    % 1.1 power
+    % 1.2 aperiodic components
+    % 1.3 r squared
+% 2. plot the whole power spectrum
+% 3. permutation test
+%% 0. Preliminaries
+
 clear all
 close all
 clc
 
-%% topoplots
-% take a data vector and plot it
-% this is EEGLAB
-% initialize EEGLAB
 proj_dir = fullfile(pwd);
 indir = fullfile(proj_dir,'data\analysis_power');
-eeglab;close;
+eeglab;close;% initialize EEGLAB, you will need it for the plots
+
 
 roi_delta = [21,102,11,37,72,36,46,79,45,19,109,24,91,90,80,89,92,93,20,47,10,56,25]; % frontal ROI (channel indices are not always the number of the actual electrode!)
 roi_beta = [82,31,62,34,87,63,1,65,3,64,2,67,71,73,78,31,34,39,83,40,84,41,85,42,86,43,74,5,75,6,7,76,8,77,68,32,69,33,70]; % put channel indices here
 
+%% 1. topoplots
+%% 1.1 power
+% take a data vector and plot it
 % for this to work you need to load in one dataset in order to have the
 % structure of the layout
 
-data_beta_c1 = importdata('data\analysis_power\export_beta_c1.txt');
-data_beta_c2 = importdata('data\analysis_power\export_beta_c2.txt');
-data_delta_c1 = importdata('data\analysis_power\export_delta_c1.txt');
-data_delta_c2 = importdata('data\analysis_power\export_delta_c2.txt');
-
-% Notes: - To change the plot map masking ring to a new figure background color,
-%            >> set(findobj(gca,'type','patch'),'facecolor',get(gcf,'color'))
-
+data_beta_pcs = importdata('data\analysis_power\export_beta_pcs.txt');
+data_beta_c = importdata('data\analysis_power\export_beta_c.txt');
+data_delta_pcs = importdata('data\analysis_power\export_delta_pcs.txt');
+data_delta_c = importdata('data\analysis_power\export_delta_c.txt');
 
 % you need to load one preprocessed data set for this in order to get
 % EEG_epoched_5.chanlocs
 
     figure;
-topoplot(data_beta_pcs.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on','emarker2', {roi_beta,'o','w',3,1})
+topoplot(data_beta_pcs.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2', {roi_beta,'o','w',3,1})
 hc=colorbar;
-caxis([0.328 2.22])
+caxis([0.346 2.97])
 xlabel(hc,'beta Power [μV^2]');
-title ('mit PCS ');
-%set(findobj(gca,'type','patch'),'facecolor', '#D94867'); % Change [0.5, 0.5, 0.5] to your desired color
+%title ('mit PCS ');
+set(findobj(gca,'type','patch'),'facecolor', '#F59541'); % Change [0.5, 0.5, 0.5] to your desired color
+set(gca, 'FontSize', 14);
 
  figure;
-topoplot(data_beta_c.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on','emarker2', {roi_beta,'o','w',3,1})
+topoplot(data_beta_c.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2', {roi_beta,'o','w',3,1})
 hc=colorbar;
-caxis([0.328 2.22])
+caxis([0.346 2.97])
 xlabel(hc,'beta Power [μV^2]');
-title ('ohne PCS ');
-%set(findobj(gca,'type','patch'),'facecolor', '#D9B348'); % Change [0.5, 0.5, 0.5] to your desired color
-
+%title ('ohne PCS ');
+set(findobj(gca,'type','patch'),'facecolor', '#02CAF5'); % Change [0.5, 0.5, 0.5] to your desired color
+set(gca, 'FontSize', 14);
 
 figure;
-topoplot(data_delta_pcs.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on','emarker2', {roi_delta,'o','w',3,1})
+topoplot(data_delta_pcs.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2', {roi_delta,'o','w',3,1})
 hc=colorbar;
-caxis([0.292 1.65])
+caxis([0.172 1.92])
 xlabel(hc,'delta Power [μV^2]');
-title ('mit PCS ');
-%set(findobj(gca,'type','patch'),'facecolor', '#9D5C99'); % Change [0.5, 0.5, 0.5] to your desired color
-
+%title ('with PCS ');
+set(findobj(gca,'type','patch'),'facecolor', '#F59541'); % Change [0.5, 0.5, 0.5] to your desired color
+set(gca, 'FontSize', 14);
 
  figure;
-topoplot(data_delta_c.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on','emarker2', {roi_delta,'o','w',3,1});
+topoplot(data_delta_c.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2', {roi_delta,'o','w',3,1});
 hc=colorbar;
-caxis([0.292 1.65]);
+caxis([0.172 1.92]);
 xlabel(hc,'delta Power [μV^2]');
-title ('ohne PCS ');
-%set(findobj(gca,'type','patch'),'facecolor', '#D9B348'); % Change [0.5, 0.5, 0.5] to your desired color
-
-% now for the aperiodic exponent
+%title ('without PCS ');
+set(findobj(gca,'type','patch'),'facecolor', '#02CAF5'); % Change [0.5, 0.5, 0.5] to your desired color
+set(gca, 'FontSize', 14);
+%% 1.2 topoplots aperiodic components
 data_ape_pcs = importdata('data\analysis_power\export_ape_pcs.txt');
 data_ape_c = importdata('data\analysis_power\export_ape_c.txt');
 data_apo_pcs = importdata('data\analysis_power\export_apo_pcs.txt');
 data_apo_c = importdata('data\analysis_power\export_apo_c.txt');
 
+% significant channel (below 5 %): 68, 86
+roi_sig = [65,83];
+% unter 10 % 1,2,5,6,7,35,43,65,69,71,72,78,79,80,86
+roi_nearly_sig = [1,2,5,6,7,32,40,62,66,68,69,75,76,77,83];
+
 figure;
-topoplot(data_ape_pcs.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
+topoplot(data_ape_pcs.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2',{roi_sig,'o','w',3,1});
 hc=colorbar;
-caxis([0.605 1.15]);
+caxis([0.648 1.16]);
 xlabel(hc,'aperiodic exponent');
+title ('with PCS ');
+set(findobj(gca,'type','patch'),'facecolor', '#F59541'); % Change [0.5, 0.5, 0.5] to your desired color
+set(gca, 'FontSize', 14);
+
+
+figure;
+topoplot(data_ape_c.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2',{roi_sig,'o','w',3,1});
+hc=colorbar;
+caxis([0.605 1.16]);
+xlabel(hc,'aperiodic exponent');
+title ('without PCS ');
+set(findobj(gca,'type','patch'),'facecolor', '#02CAF5'); % Change [0.5, 0.5, 0.5] to your desired color
+set(gca, 'FontSize', 14);
+
+figure;
+topoplot(data_apo_pcs.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on');
+hc=colorbar;
+caxis([-0.630 -0.0323]);
+xlabel(hc,'aperiodic offset');
+title ('with PCS ');
+
+figure;
+topoplot(data_apo_c.data,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on');
+hc=colorbar;
+caxis([-0.630 -0.0323]);
+xlabel(hc,'aperiodic offset');
+title ('without PCS ');
+
+   
+%% 1.3 topoplot r squared
+data_r_pcs = importdata('data\analysis_power\export_r_pcs.txt');
+data_r_c = importdata('data\analysis_power\export_r_c.txt');
+
+
+figure;
+topoplot(data_r_pcs.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
+hc=colorbar;
+caxis([0.833 0.959]);
+xlabel(hc,'r squared');
 title ('mit PCS ');
 
 figure;
-topoplot(data_ape_c.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
+topoplot(data_r_c.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
 hc=colorbar;
-caxis([0.605 1.15]);
-xlabel(hc,'aperiodic exponent');
-title ('ohne PCS ');
+caxis([0.833 0.959]);
+xlabel(hc,'R^2');
+%set(findobj(gca,'type','patch'),'facecolor', '#FFFFFF'); % Change [0.5, 0.5, 0.5] to your desired color
 
-figure;
-topoplot(data_apo_pcs.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
-hc=colorbar;
-caxis([0 0.62]);
-xlabel(hc,'aperiodic offset');
-title ('mit PCS ');
 
-figure;
-topoplot(data_apo_c.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
-hc=colorbar;
-caxis([0 0.62]);
-xlabel(hc,'aperiodic offset');
-title ('ohne PCS ');
-
-% plot the whole spectrum!
+%% 2. plot the whole spectrum!
 % for this I need the groups again
 % load the age matched data
 data_behav = readtable("C:\Users\Lara Godbersen\Documents\GitHub\Masters-thesis\data\PuG\matched_participants_conn.tsv", "FileType","text",'Delimiter', '\t');
@@ -107,9 +145,11 @@ withPCS = table2array(data_behav(1:23,1));
 withoutPCS = table2array(data_behav(24:46,1));
 
 % trying to calculate a mean of the matrices
-M_pcs = zeros(126,31);
-M_c = zeros(126,31);
-M_rest = zeros(126,31);
+M_pcs = zeros(126,149);
+M_c = zeros(126,149);
+M_rest = zeros(126,149);
+
+% load the results of the power analysis: original, oscillatory, fractal
 
 for i = 1:length(oscillatory)
     participant_id =  oscillatory{i}.id;
@@ -126,7 +166,7 @@ end
 avg_power_pcs = M_pcs/23;
 avg_power_c = M_c/23;
 
-v = 58;
+v = 1;
  hold on;
     plot((original{1,1}.freq),avg_power_pcs,'k');
     plot((original{1,1}.freq),avg_power_c,'b');
@@ -146,7 +186,7 @@ frontal_roi_pcs = mean(avg_power_pcs(roi_delta,:));
 central_roi_pcs = mean(avg_power_pcs(roi_beta,:));
 
 hold on;
-    plot((original{1,1}.freq),frontal_roi_c,'k');
+    plot((original{1,1}.freq),frontal_roi_c,'k');% I used 'original' here just to get the freq, the data shown is from oscillatory!
     plot((original{1,1}.freq),frontal_roi_pcs,'b');
     
 hold on;
@@ -198,31 +238,8 @@ ylabel('log(power)');
 % Set the font size for the axis ticks
 set(gca, 'FontSize', 14);
 
-    
-    
-% plot the squared r
-data_r_pcs = importdata('data\analysis_power\export_r_pcs.txt');
-data_r_c = importdata('data\analysis_power\export_r_c.txt');
-
-
-figure;
-topoplot(data_r_pcs.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
-hc=colorbar;
-caxis([0.802 0.963]);
-xlabel(hc,'r squared');
-title ('mit PCS ');
-
-figure;
-topoplot(data_r_c.data,EEG_epoched_5.chanlocs,'colormap',cividis,'electrodes','on');
-hc=colorbar;
-caxis([0.802 0.963]);
-xlabel(hc,'R^2');
-set(findobj(gca,'type','patch'),'facecolor', '#FFFFFF'); % Change [0.5, 0.5, 0.5] to your desired color
-
-
-
-
-%% permutation test (try)
+%% 3. permutation test
+% add group to every individual data set
 for i = 1:length(oscillatory)
     participant_id =  oscillatory{i}.id;
     
@@ -235,12 +252,10 @@ for i = 1:length(oscillatory)
     end
 end
 
-
 % Initialize empty arrays for P and W groups
     oscillatory_P = struct([]);
     oscillatory_W = struct([]);
 % Loop through the structure array and separate based on group
-
 for i = 1:length(oscillatory)
     if strcmp(oscillatory{i}.group, 'withPCS')
         oscillatory_P = [oscillatory_P, oscillatory{i}];
@@ -262,29 +277,30 @@ for i = 1:length(oscillatory)
     end
 end
 
-% layout
- elec = fullfile(proj_dir,'Src\BC-128-pass-lay.mat');  
-    cfg = [];
-    cfg.elec = elec;
-    layout = ft_prepare_layout(cfg);
+% load the layout
+elec = fullfile(proj_dir,'Src\BC-128-pass-lay.mat');  
+cfg = [];
+cfg.elec = elec;
+layout = ft_prepare_layout(cfg);
     
-    % prepare neighbours
-    channels_to_exclude = {'31', '32'};
-    all_channels = layout.label; % Get all channel labels from the layout
+% prepare neighbours
+channels_to_exclude = {'31', '32'};
+all_channels = layout.label; % Get all channel labels from the layout
 channels_to_include = setdiff(all_channels, channels_to_exclude);
 
     
 cfg = []; 
-        cfg.method = 'distance'; % how should the neighbors be selected?
-        cfg.channel = channels_to_include;
-        %cfg.neighbourdist = 3; % I have no Idea what range this has, just make sure, that you get meaningful neighbors
-        cfg.elec = elec; % Here we need the 3d-positions!
+cfg.method = 'distance'; % how should the neighbors be selected?
+cfg.channel = channels_to_include;
+%cfg.neighbourdist = 3; % I have no Idea what range this has, just make sure, that you get meaningful neighbors
+cfg.elec = elec; % Here we need the 3d-positions!
         
-        neigh = ft_prepare_neighbours(cfg); % between 5 and 10 neighbors is a good value, always good to check!
+neigh = ft_prepare_neighbours(cfg); % between 5 and 10 neighbors is a good value, always good to check!
 
+% now the actual permutation test with clusters
 cfg = [];
 %cfg.latency          = 'all';
-cfg.frequency        = [1 30];% get f.ex. delta here
+cfg.frequency        = [0.6 4];% [0.5 4] for delta [14 30] for beta
 cfg.avgoverfreq      = 'no';
 cfg.method           = 'montecarlo';
 cfg.statistic        = 'ft_statfun_indepsamplesT';
@@ -296,7 +312,7 @@ cfg.minnbchan        = 2;
 cfg.tail             = 0;
 cfg.clustertail      = 0;
 cfg.alpha            = 0.05;
-cfg.numrandomization = 500;
+cfg.numrandomization = 1000;
 % prepare_neighbours determines what sensors may form clusters
 cfg.neighbours       = neigh;
 
@@ -307,12 +323,58 @@ design(1,(length(oscillatory_W)+1):(length(oscillatory_W)+length(oscillatory_P))
 cfg.design           = design;
 cfg.ivar             = 1;
 
-[stat] = ft_freqstatistics(cfg, oscillatory_W{:}, oscillatory_P{:});
-
-cfg = [];
-        cfg.layout = layout;
-        cfg.parameter = 'stat';
-        cfg.maskparameter = 'mask';
-
-        ft_multiplotER(cfg,stat);
+[stat_delta] = ft_freqstatistics(cfg, oscillatory_W{:}, oscillatory_P{:});
         
+% get the topography of stat
+plot_stat_delta = mean(stat_delta.stat,2);
+
+figure;
+topoplot(plot_stat_delta,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2', {roi_delta,'o','w',3,1});
+hc=colorbar;
+caxis([-0.585 1.33]);
+xlabel(hc,'\it{t} - value');
+%title ('without PCS ');
+set(gca, 'FontSize', 15);
+set(findobj(gca,'type','patch'),'facecolor', '#FFFFFF'); % Change [0.5, 0.5, 0.5] to your desired color
+
+
+% now beta
+% now the actual permutation test with clusters
+cfg = [];
+%cfg.latency          = 'all';
+cfg.frequency        = [14 30];% [0.5 4] for delta [14 30] for beta
+cfg.avgoverfreq      = 'no';
+cfg.method           = 'montecarlo';
+cfg.statistic        = 'ft_statfun_indepsamplesT';
+cfg.parameter        = 'powspctrm';
+cfg.correctm         = 'cluster';
+cfg.clusteralpha     = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan        = 2;
+cfg.tail             = 0;
+cfg.clustertail      = 0;
+cfg.alpha            = 0.025;
+cfg.numrandomization = 1000;
+% prepare_neighbours determines what sensors may form clusters
+cfg.neighbours       = neigh;
+
+design = zeros(1,length(oscillatory_W) + length(oscillatory_P));
+design(1,1:length(oscillatory_W)) = 1;
+design(1,(length(oscillatory_W)+1):(length(oscillatory_W)+length(oscillatory_P))) = 2;
+
+cfg.design           = design;
+cfg.ivar             = 1;
+
+[stat_beta] = ft_freqstatistics(cfg, oscillatory_W{:}, oscillatory_P{:});
+
+plot_stat_beta = mean(stat_beta.stat,2);
+        
+% get the topography of stat
+figure;
+topoplot(plot_stat_beta,EEG_epoched_5.chanlocs,'colormap',viridis,'electrodes','on','emarker2', {roi_beta,'o','w',3,1});
+hc=colorbar;
+caxis([-1.3711 1.0170]);
+xlabel(hc,'\it{t} - value');
+%title ('without PCS ');
+set(gca, 'FontSize', 15);
+set(findobj(gca,'type','patch'),'facecolor', '#FFFFFF'); % Change [0.5, 0.5, 0.5] to your desired color
